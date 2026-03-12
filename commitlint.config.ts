5f2dfb84933d48418071911c919349f0ddf99efe
sergey-s-betke <sergey.s.betke@yandex.ru>
2026-03-12 11:28:53 +0300

    chore: add commitlint config

---
+++ commitlint.config.ts
@@ -0,0 +1,29 @@
+import { type UserConfig, RuleConfigSeverity } from '@commitlint/types';
+
+// eslint-disable-next-line max-len
+// https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/config-conventional
+// https://commitlint.js.org/reference/configuration.html
+
+const Configuration: UserConfig = {
+
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [RuleConfigSeverity.Error, 'always', [
+      'plugin',
+      'vscode',
+      'git',
+      'github',
+      'github-actions',
+      'deps',
+      'readme',
+      'changelog'
+    ]]
+  },
+  defaultIgnores: true,
+  ignores: [
+    (commit) => commit.startsWith('build(deps): bump'),
+    (commit) => commit.startsWith('ci(deps): bump')
+  ],
+};
+
+export default Configuration;
